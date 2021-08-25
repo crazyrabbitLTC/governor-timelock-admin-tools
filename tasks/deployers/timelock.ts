c2c2695d46a772eb4f7312b3033dd4fdefbf2afc
Dennison Bertram <dennison@dennisonbertram.com>
2021-08-25 18:59:33 -0400

    feat: create deploy task

--- tasks/deployers/timelock.ts
+++ tasks/deployers/timelock.ts
@@ -1,25 +1,59 @@
 import { task } from "hardhat/config";
-import { TaskArguments } from "hardhat/types";
+import { TaskArguments, Artifact } from "hardhat/types";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 
 import { Timelock, Timelock__factory } from "../../typechain";
 
-// function encodeParameters(types, values) {
-//   const abi = new ethers.utils.AbiCoder();
-//   return abi.encode(types, values);
-// }
+task("deploy:Timelock")
+  .addParam("delay", "timelock delay")
+  .addParam("admin", "admin address")
+  .setAction(async function (taskArgs: TaskArguments, { ethers }) {
+    // If you want to hardcode
+    // const delay = 259200;
+    // const signers: SignerWithAddress[] = await ethers.getSigners();
+    // const admin = await signers[0].getAddress();
 
-task("deploy:Timelock").setAction(async function (taskArguments: TaskArguments, { ethers }) {
-  const delay = 259200;
+    const timelockFactory: Timelock__factory = await ethers.getContractFactory("Timelock");
+    const timelock: Timelock = <Timelock>await timelockFactory.deploy(taskArgs.admin, taskArgs.delay);
 
-  const signers: SignerWithAddress[] = await ethers.getSigners();
-  const admin = await signers[0].getAddress();
+    await timelock.deployed();
+    console.log("Timelock deployed to: ", timelock.address);
+  });
 
-  const timelockFactory: Timelock__factory = await ethers.getContractFactory("Timelock");
-  const timelock: Timelock = <Timelock>await timelockFactory.deploy(admin, delay);
+task("queue:tx")
+  .addParam("timelock", "timelock address")
+  .addParam("target", "target address")
+  .addParam("value", "eth to send")
+  .addParam("signature", "function signature")
+  .addParam("argTypes", "array of argument types")
+  .addParam("argValues", "array of argument values")
+  .addParam("eta", "eta for tx to execute")
+  .setAction(async function (taskArgs: TaskArguments, hre) {
+    const { ethers } = hre;
+    const signer: SignerWithAddress = (await ethers.getSigners())[0];
+    const timelockArtifact: Artifact = await hre.artifacts.readArtifact("Timelock");
+    const timelock: Timelock = <Timelock>new ethers.Contract(taskArgs.timelock, timelockArtifact.abi, signer);
 
-  await timelock.deployed();
-  console.log("Timelock deployed to: ", timelock.address);
-});
+    // not in WEI!!!!
+    const sendValue = ethers.utils.parseEther(taskArgs.value);
 
-// task("timelock:sendEth").addParam("ether", "ether Amount").setAction
+    const sendData = new ethers.utils.AbiCoder().encode(taskArgs.argTypes, taskArgs.argValues);
+
+    await timelock.queueTransaction(taskArgs.target, sendValue, taskArgs.signature, sendData, taskArgs.eta);
+
+    const expectedHash = ethers.utils.defaultAbiCoder.encode(
+      ["address", "uint256", "string", "bytes", "uint256"],
+      [taskArgs.target, sendValue, taskArgs.signature, sendData, taskArgs.eta],
+    );
+
+    console.log("Queued TX id: ", expectedHash);
+
+    // verify
+    const verified = await timelock.queuedTransactions(expectedHash);
+
+    if (verified) {
+      console.log("Succesfully Queued");
+    } else {
+      console.log("Failed to queue");
+    }
+  });
